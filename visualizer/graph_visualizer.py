afab21b71a3dc6f22cf1f75091ff145c38ab3d9a
Bastiche <bastien.velitchkine@live.fr>
2022-04-01 10:08:50 +0200

    📝 Enhanced docstrings

--- visualizer/graph_visualizer.py
+++ visualizer/graph_visualizer.py
@@ -2,6 +2,8 @@ import networkx as nx
 
 def print_graph(graph):
     '''
-    A function that displays a graph (a small one though)
+    A function that displays a graph (a small one though) with
+    networkx's built-in method ```networkx.draw```
     '''
     nx.draw(graph)
+    
--- visualizer/image_visualization.py
+++ visualizer/image_visualization.py
@@ -2,14 +2,14 @@ import matplotlib.pyplot as plt
 
 def print_grayscale(img):
     '''
-    A function to print a grayscale image
+    A function to print a grayscale image with ```matplotlib.pyplot```.
     '''
     plt.imshow(img, cmap="gray", vmin=0, vmax=255)
     plt.show()
 
 def print_rgb(img):
     '''
-    A function to print a grayscale image
+    A function to print a rgb image with ```matplotlib.pyplot```.
     '''
     plt.imshow(img)
     plt.show()
